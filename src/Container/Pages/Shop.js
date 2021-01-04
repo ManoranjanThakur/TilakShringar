94bb41649448c77cef9234318d7e612657827955
Manoranjan Kumar Thakur <67204309+ManoranjanThakur@users.noreply.github.com>
2021-01-04 22:44:54 +0530

    Update Shop.js

--- src/Container/Pages/Shop.js
+++ src/Container/Pages/Shop.js
@@ -1,62 +1,85 @@
-import React from 'react';
-import Header from '../../Container/Top Nav Bar/Header';
-import Index2 from '../../Container/Side Nav Bar/Index2';
-import Search from '../Search Button/Search';
-import Footer from '../Footer/Footer';
-import './Shop.css';
-import {TiHeartFullOutline} from "react-icons/ti";
+import React from "react";
+import Header from "../../Container/Top Nav Bar/Header";
+import Index2 from "../../Container/Side Nav Bar/Index2";
+import Search from "../Search Button/Search";
+import Footer from "../Footer/Footer";
+import "./Shop.css";
+import { TiHeartFullOutline } from "react-icons/ti";
 import { BiCartAlt } from "react-icons/bi";
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom'
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Shop = (props) => {
-    const product = useSelector(state => state.product);
-    const renderProducts = () => {
-        return (
-            <div style={{marginLeft: '21.474vw'}}>
-                { 
-                    product.products.length > 0 ?
-                        product.products.map(product =>
-                            <div>
-                                <div className='product1'>
-                                    <div className="ii"> 
-                                    <Link to={`/${product.slug}/${product._id}/p`} style = {{
-                                display:'block'
-                            }}><img src={product.productPictures.img} alt='HR'/></Link>
-                                        <div className="ii1"><BiCartAlt id="iii1"/></div> 
-                                        <div className="ii2"><TiHeartFullOutline id="iii2"/></div> 
-                                    </div>
-                                </div>
-                                <div className='p1info'>
-                                <Link to={`/${product.slug}/${product._id}/p`} style = {{
-                                display:'block'
-                            }}>
-                                    <p2 style={{color:'#4D4D4D'}} id='product1i'>{product.name}</p2></Link>
+  const product = useSelector((state) => state.product);
+  const renderProducts = () => {
+    return (
+      <div
+        style={{ marginLeft: " 21.474vw", display: " flex", flexWrap: "wrap" }}
+      >
+        {product.products.length > 0 ? (
+          product.products.map((product) => (
+            <div
+              style={{
+                display: "grid",
+                height: "30vw",
+                width: "24.158vw",
+                marginLeft: "1vw",
+                marginBottom: " 3.5vw",
+              }}
+            >
+              <div className="product1">
+                <Link
+                  to={`/${product.slug}/${product._id}/p`}
+                  style={{
+                    display: "block",
+                  }}
+                >{product.productPictures.map((picture) => (
+                  <img src={`http://139.59.92.185:2000/public/${picture.img}`} alt="HR" />
+              ))}
+                </Link>
+                <div
+                  style={{
+                    position: "absolute",
+                    display: "flex",
+                    left: "18.409vw",
+                    top: "0.366vw",
+                  }}
+                >
+                  <div className="ii2">
+                    <TiHeartFullOutline id="iii2" />
+                  </div>
+                  <div className="ii1">
+                    <BiCartAlt id="iii1" />
+                  </div>
+                </div>
+              </div>
+              <div className='p1info'>
+                                    <Link to={`/${product.slug}/${product._id}/p`} style={{
+                                        display: 'block'
+                                    }}>
+                                        <p2 style={{ color: '#4D4D4D' }} id='product1i'>{product.name}</p2></Link>
                                     <p2 id='price1'>Rs. {product.price}/-</p2>
                                 </div>
-                            </div>
-                        ) : 
-                            
-                            <div className="nullProducts">
-                                <h2>No Products</h2>
-                            </div>
-                            
-        
-    
-                }
             </div>
-        )
-    }
-    return (
-        <div>
-            <Header/>
-            <Index2/>
-            <div className='shop'>
-                < Search />
-            </div>
-            <h2 className='shopheadlinee'>our products...</h2>
-            {renderProducts()}
-            {/* <div className='product1'>
+          ))
+        ) : (
+          <div className="nullProducts">
+            <h2>No Products</h2>
+          </div>
+        )}
+      </div>
+    );
+  };
+  return (
+    <div>
+      <Header />
+      <Index2 />
+      <div className="shop">
+        <Search />
+      </div>
+      <h2 className="shopheadlinee">our products...</h2>
+      {renderProducts()}
+      {/* <div className='product1'>
                 <div className="ii"> 
                     <div className="ii1"><BiCartAlt id="iii1"/></div> 
                     <div className="ii2"><TiHeartFullOutline id="iii2"/></div> 
@@ -66,7 +89,7 @@ const Shop = (props) => {
                 <p2 id='product1i'><a href='/product' style={{color:'#4D4D4D'}}>Product Name will be written in maximum of two lines</a></p2>
                 <p2 id='price1'>Rs. 300/-</p2>
             </div> */}
-            {/* <div className='product2'>
+      {/* <div className='product2'>
                 <div className="ii"> 
                     <div className="ii1"><BiCartAlt id="iii1"/></div> 
                     <div className="ii2"><TiHeartFullOutline id="iii2"/></div> 
@@ -133,7 +156,7 @@ const Shop = (props) => {
                 <p2 id='price9'>Rs. 300/-</p2>
             </div>
 
-            <button className="shoploadMore">Load More </button>
+            <button className="shoploadMore">Load More </button> */}
             <div className='cards'>
                 <div className='rrcard3'>
                     <span><a href="/categories" style={{color:'#4D4D4D'}}>CATEGORIES</a></span>
@@ -141,10 +164,10 @@ const Shop = (props) => {
                 <div className='rrcard4'>
                     <span><a href="/query" style={{color:'#4D4D4D'}}>QUERY</a></span>
                 </div>
-            </div> */}
-            <Footer/>
-        </div>
-    )
-}
+            </div>
+      <Footer />
+    </div>
+  );
+};
 
-export default Shop
+export default Shop;
