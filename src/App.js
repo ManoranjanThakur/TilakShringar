fc07018e2824341d48094aa3384918c05279f500
Manoranjan Kumar Thakur <67204309+ManoranjanThakur@users.noreply.github.com>
2021-04-29 00:24:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import './App.css';
+
 import 'semantic-ui-css/semantic.css'
 import Home from './Container/Pages/Home';
 import { BrowserRouter as Router , Switch, Route} from 'react-router-dom';
